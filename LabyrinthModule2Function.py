a7229b21bbae5cbf5a102b4ec428f3e8d44c8369
dank_memes <cecily.tighe@gmail.com>
2016-08-02 23:59:40 +1000

    Add files via upload

--- LabyrinthModule2Function.py
+++ LabyrinthModule2Function.py
@@ -2,11 +2,14 @@
 # Cecily Tighe
 # 2016
 
+#contains most of the functionality
+
 import xml.etree.ElementTree as ET
 import time
 #import sphero_driver as sphero
-#import LabyrinthModule2GUI as GUI
 from Tkinter import *
+from displayNames import lstDisplayLBLNames
+
 
 class Functions:
 
@@ -26,49 +29,9 @@ class Functions:
     def fnSpheroStart(self):
 
         speed = 0.35
-        time_one_grid = 1
+        time_one_grid = 0.125
 
-        dictTime = {}
-
-        routeArray = [[1, 7, 13, 14, 15, 16, 17, 23, 24],
-                      [1, 7, 13, 14, 15, 16, 17, 11, 12, 18, 24]]
-
-
-        """ 1. All routes are retrieved from an xml file
-        input: -
-        output: 2D array of all routes"""
-
-
-
-
-
-
-
-        """ 2.Approximate time of route length is calculated and appended to a dictionary along with the actual route
-        input: list of routes
-        output: dictionary of route with time
-        eg:
-        dictRoute = {[route]:450 }
-        """
-
-        for eachRoute in routeArray:
-        #create a new item in the dictionary of route to route times
-        #length of time is equal to the amount of sub grids crossed multiplied
-        #by the time it takes to cross 1 grid in seconds at speed 1.4 m/s
-            eachRoute = tuple(eachRoute)
-            approx_length = len(eachRoute) * time_one_grid
-            dictTime[approx_length] = eachRoute
-        print 'dict: ' + str(dictTime)
-        
-
-
-        """3. From the list of times, all times are sorted and the lowest time returned
-        input: dictionary of route times with actual route
-        output: best time"""
-
-        lstTime = sorted(dictTime, key=dictTime.get)
-        bestRoute = dictTime[lstTime[0]]
-        print bestRoute
+        #bestRoute = visited points
 
         """4. from the best route, Sphero specific instructions are generated
         input:best route
@@ -81,46 +44,51 @@ class Functions:
 
         # set the initial clock
         startTime = time.clock()
-
+        index = 0
+        bestRoute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 24, 36, 48, 60, 72, 84, 96]
+        print bestRoute
         for eachStep in bestRoute:
+
             # if the next grid visited is equal to current grid + 1, then it must be located to the right
-            if bestRoute[eachStep+1] == bestRoute[eachStep] + 1:
-                while True:
-                    if round(time.clock(),2) < round(startTime + time_one_grid, 2):
-                        #sphero.roll(50, 0, 1, True)
-                        print 'right'
-                    else:
-                        break
+            try:
+                if bestRoute[index + 1] == bestRoute[index] + 1:
+                    while True:
+                        if round(time.clock(), 2) < round(startTime + 0.5 * index, 2):
+                            # sphero.roll(50, 0, 1, True)
+                            print 'right'
+                        else:
+                            break
 
-            # if the next grid visited is equal to current grid + 1, then it must be located to the left
-            elif bestRoute[eachStep+1] == bestRoute[eachStep] - 1:
-                while True:
-                    if round(time.clock(),2) < round(startTime + time_one_grid):
-                        #sphero.roll(50, 180, 1, True)
-                        print 'left'
-                    else:
-                        break
+                # if the next grid visited is equal to current grid + 1, then it must be located to the left
+                elif bestRoute[index + 1] == bestRoute[index] - 1:
+                    while True:
+                        if round(time.clock(), 2) < round(startTime + 0.5 * index, 2):
+                            # sphero.roll(50, 180, 1, True)
+                            print 'left'
+                        else:
+                            break
 
-            # if the next grid visited is equal to current grid + 1, then it must be located downwards
-            elif bestRoute[eachStep+1] == bestRoute[eachStep] + 7:
-                while True:
-                    if round(time.clock(),2) < round(startTime + time_one_grid):
-                        #sphero.roll(50, 270, 1, True)
-                        print 'up'
-                    else:
-                        break
+                # if the next grid visited is equal to current grid + 12, then it must be located downwards
+                elif bestRoute[index + 1] == bestRoute[index] - 12:
+                    while True:
+                        if round(time.clock(), 2) < round(startTime + 0.5 * index, 2):
+                            # sphero.roll(50, 270, 1, True)
+                            print 'up'
+                        else:
+                            break
 
-            # if the next grid visited is not equal to any of the earlier options, it must be located upwards
-            else:
-                while True:
-                    if round(time.clock(),2) < round(startTime + time_one_grid):
-                        #sphero.roll(50, 270, 1, True)
-                        print 'down'
-                    else:
-                        break
-                
-            
-                
+                # if the next grid visited is not equal to any of the earlier options, it must be located upwards
+                elif bestRoute[index + 1] == bestRoute[index] + 12:
+                    while True:
+                        if round(time.clock(), 2) < round(startTime + 0.5 * index, 2):
+                            # sphero.roll(50, 270, 1, True)
+                            print 'down'
+                        else:
+                            break
+                index += 1
+            # the end of the route
+            except IndexError:
+                pass
                 
                 
             
@@ -129,49 +97,174 @@ class Functions:
         """also stop display"""
         #sphero.disconnect(self)
 
+
     """upon mouse click 'stop', uses an arrangement of pictures to replicate the route the Sphero is following
         input: best route instructions
         output: display is updated"""
-    def fnUpdateDisplay(self, bestRoute):
+
+    def fnUpdateDisplay(self):
+
         """using list of pictures to replicate route
             wait amount of seconds, display the next picture"""
-        ld = PhotoImage(file = 'left_down.gif')
-        lu = PhotoImage(file = 'left_up.gif')
-        ru = PhotoImage(file = 'right_up.gif')
-        rd = PhotoImage(file = 'right_down.gif')
-        hor = PhotoImage(file = 'horizontal.gif')
-        ver = PhotoImage(file = 'vertical.gif')
+        leftUp = PhotoImage(file="left_up.gif")
+        leftDown = PhotoImage(file="left_down.gif")
+        rightDown = PhotoImage(file='right_down.gif')
+        rightUp = PhotoImage(file='right_up.gif')
+        hor = PhotoImage(file='horizontal.gif')
+        ver = PhotoImage(file='vertical.gif')
+        ob3 = PhotoImage(file='ob3.gif')
+        ob3 = PhotoImage(file='ob3.gif')
+        ob1 = PhotoImage(file='ob1.gif')
 
-        """for eachStep in self.bestRoute:
-            if eachStep == 'r':
-                GUI.lstDisplayLBLNames[eachStep].config(image = hor)
-                
-            elif eachStep == 'l':
-                GUI.lstDisplayLBLNames[eachStep].config(image = hor)
-            elif eachStep == 'u':
-                while True:
-                    if int(time.clock()) < int(starttime + self.time):
-                        sphero.roll(50, 270, 1, True)
-                    else:
-                        break
-            elif eachStep == 'd':
-                while True:
-                    if int(time.clock()) < int(starttime + self.time):
-                        sphero.roll(50, 270, 1, True)
-                    else:
-                        break"""
-            
-            
+        bestRoute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 24, 36, 48, 60, 72, 84, 96]
+        dictMaze = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 11, 12,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    'Z', 'A', 'A', 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+        # set picture for obstacles
+        # this happens first as it is not reliant upon time
 
-        
+        index = 0
+        for eachCell in dictMaze:
+            index += 1
+            print eachCell
 
-        
 
-    """when return is selected in menubar, the window closes and module 1 is opened"""
-    def fnReturnModule1(self):
-        """do the thing"""
- 
+            if str(eachCell )== 'Z' or 'A':
+                lstDisplayLBLNames[index].config(image=hor)
+                lstDisplayLBLNames[index].image = hor
 
-  #  fnRouteTimes({1 : [1,7,13,14,15,16,17, 23,24],2:[1,7,13,14,15,16,17, 11,12, 18,24]}, speed)
+            elif eachCell == 'Y' or 'B':
+                ob2 = PhotoImage(file='ob2.gif')
+                lstDisplayLBLNames[index].config(image=ob2)
+                lstDisplayLBLNames[index].image=ob2
 
-    fnSpheroStart(0)
+            elif eachCell == 'X' or 'C':
+
+                lstDisplayLBLNames[index].config(image=ob1)
+                lstDisplayLBLNames[index].image=ob1
+
+
+
+        index = -1
+
+        startTime = time.time()
+        for eachStep in bestRoute:
+            index += 1
+
+
+
+
+
+            while True:
+                nowTime = time.time()
+                timeElapsed = nowTime - startTime
+
+                if timeElapsed > 0.5 *(index + 1):
+                    try:
+                        print 'trying'
+
+                        # last grid is to the left of the current grid
+                        if bestRoute[index - 1] == bestRoute[index] - 1:
+                            # next grid is above current grid but last grid was to the left
+                            if bestRoute[index + 1] == bestRoute[index] - 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=leftUp)
+                                lstDisplayLBLNames[eachStep - 1].image = leftUp
+
+
+                            # next grid is below current grid but last grid was the left
+                            elif bestRoute[index + 1] == bestRoute[index] + 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=leftDown)
+                                lstDisplayLBLNames[eachStep - 1].image = leftDown
+
+                            # next grid is to the right of the current grid but last grid was to the left
+                            elif bestRoute[index + 1] == bestRoute[index] + 1:
+                                lstDisplayLBLNames[eachStep - 1].config(image=hor)
+                                lstDisplayLBLNames[eachStep - 1].image = hor
+                                print 'y'
+
+
+
+
+                        # last grid is to the right of the current grid
+                        elif bestRoute[index - 1] == bestRoute[index] + 1:
+                            # next grid is above the current grid but last grid was to the right
+                            if bestRoute[index + 1] == bestRoute[index] - 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=rightUp)
+                                lstDisplayLBLNames[eachStep - 1].image = rightUp
+
+                            # next grid is below the current grid but last grid was to the right
+                            elif bestRoute[index + 1] == bestRoute[index] + 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=rightDown)
+                                lstDisplayLBLNames[eachStep - 1].image = rightDown
+
+
+                            # last grid was to the right and next grid is to the left of the current grid
+                            elif bestRoute[index + 1] == bestRoute[index] - 1:
+                                lstDisplayLBLNames[eachStep - 1].config(image=hor)
+                                lstDisplayLBLNames[eachStep - 1].image = hor
+
+
+
+                        # last grid is above of the current grid
+                        if bestRoute[index - 1] == bestRoute[index] - 12:
+                            # next grid is to the left of current grid, last grid was above current grid
+                            if bestRoute[index + 1] == bestRoute[index] - 1:
+                                lstDisplayLBLNames[eachStep - 1].image = leftUp
+
+                            # next grid is to the right of current grid, last grid was above current grid
+                            elif bestRoute[index + 1] == bestRoute[index] + 1:
+                                lstDisplayLBLNames[eachStep - 1].config(image=rightUp)
+                                lstDisplayLBLNames[eachStep - 1].image = rightUp
+
+                            # next grid is below current grid, last grid was above current grid
+                            elif bestRoute[index + 1] == bestRoute[index] + 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=ver)
+                                lstDisplayLBLNames[eachStep - 1].image = ver
+
+
+                        # last grid is below the current grid
+                        if bestRoute[index - 1] == bestRoute[index] + 12:
+                            # next grid is to the left of current grid, last grid was below current grid
+                            if bestRoute[index + 1] == bestRoute[index] + 1:
+                                lstDisplayLBLNames[eachStep - 1].config(image=leftDown)
+                                lstDisplayLBLNames[eachStep - 1].image = leftDown
+
+                                # next grid is to the right of current grid, last grid was below
+                            elif bestRoute[index + 1] == bestRoute[index] - 1:
+                                lstDisplayLBLNames[eachStep - 1].config(image=rightDown)
+                                lstDisplayLBLNames[eachStep - 1].image = rightDown
+
+                            # next grid is above of current grid, last grid was below current grid
+                            elif bestRoute[index + 1] == bestRoute[index] - 12:
+                                lstDisplayLBLNames[eachStep - 1].config(image=ver)
+                                lstDisplayLBLNames[eachStep - 1].image = ver
+
+                    # there is no 'last grid' - the current grid is the start route
+                    # or
+                    # there is no 'next grid'- current grid is the end route
+                    except IndexError:
+                        # init = PhotoImage(file = 'start.gif')
+                        lstDisplayLBLNames[eachStep - 1].config(image=ver)
+                        lstDisplayLBLNames[eachStep - 1].image = ver
+                    break
+
+                else:
+                    pass
+        print lstDisplayLBLNames
+
+
+
+
+
+
+
+
+
+
+
+
--- LabyrinthModule2GUI.py
+++ LabyrinthModule2GUI.py
@@ -2,15 +2,29 @@
 # Cecily Tighe
 # 2016
 
+# contains display and small display related functions
+
 from Tkinter import *
+from displayNames import lstDisplayLBLNames
+import LabyrinthModule2Function
+import time
+
+fn = LabyrinthModule2Function.Functions()
 
 
 class GUI:
+
     def __init__(self, master):
         self.master = master
         self.master.title("Labyrinth Maze Solve")
         self.master.minsize(700, 550)
+        self.master.maxsize(700, 550)
         self.fnCreateWidgets()
+
+        lstDisplayLBLNames = []
+
+#define the names of the labels that are used in both files and make them global
+
 
     def fnCreateWidgets(self):
         # set background color
@@ -24,56 +38,52 @@ class GUI:
         # set basic fontsize
         fontSize = 12
         # create the list of names for the labels in the display
-        self.lstDisplayLBLNames = []
+
 
         # create the frame
         self.frWindow = Frame(height=400, width=500, bg=bg)
         self.frWindow.pack(fill=BOTH, expand=1)
-        
+
+        #background image
+        background_image=PhotoImage(file = 'background.gif')
+        background_label = Label(self.frWindow, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        background_label.image = background_image
 
         #menu bar
-        
-        menubar = Menu(wdBaseWindow)
-        menubar.add_command(label="File")
-        menubar.add_command(label="Quit", command=wdBaseWindow.quit())
-
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label='Return to Route Plotter', command=self.fnReturnModule1)
+        fileMenu.add_command(label="Quit", command = self.fnQuit)
+        menubar.add_cascade(label="File", menu=fileMenu)
         wdBaseWindow.config(menu=menubar)
 
-        # blank labels for spacing
-        self.lbl1 = Label(self.frWindow, bg = bg)
-        self.lbl1.grid(row =0)
-
-        self.lbl1 = Label(self.frWindow, bg = bg)
-        self.lbl1.grid(row =6)
-
-        self.lbl2 = Label(self.frWindow, width = 2, bg =bg)
-        self.lbl2.grid(column = 0)
-
-        self.lbl3 = Label(self.frWindow, width = 2, bg =bg)
-        self.lbl3.grid(column = 1)
-
         # label, will display the Sphero runtime
-        self.lblTimeElapsed = Label(self.frWindow, text = 'Time:')
-        self.lblTimeElapsed.grid(row=1, column =6)
+        self.lblTime = Label(self.frWindow, text = 'Time:')
+        self.lblTime.grid(row=1, column =10, pady = (20,10))
+
+        self.lblTimeElapsed = Label(self.frWindow, text='00:00')
+        self.lblTimeElapsed.grid(row=1, column=11, pady=(20, 10))
 
         # button, starts the run
-        self.btnStart = Button(self.frWindow, text="Start", font=(font,16))
-        self.btnStart.grid(row=7, column=2)
+        self.btnStart = Button(self.frWindow, text="Start", command =self.fnStart,font=(font,16))
+        self.btnStart.grid(row=10, column=3, pady = (30,0))
 
         # button, stops the run completely - NOT pause
         self.btnStop = Button(self.frWindow, text="Stop", font=(font,16))
-        self.btnStop.grid(row=7, column=7)
+        self.btnStop.grid(row=10, column=10, pady = (30,0))
 
         """DISPLAY Grid Arrangement:
         4x6
-                0      1     2
-               0  1   2  3   4  5
-         0  0 |X||X| |X||X| |X||X|
+
+               0  1   2  3   4  5 ... 12
+            0 |X||X| |X||X| |X||X|
             1 |X||X| |X||X| |X||X|
 
-         1  2 |X||X| |X||X| |X||X|
+            2 |X||X| |X||X| |X||X|
             3 |X||X| |X||X| |X||X|
-
+            ...8
         Labels that hold images of different route combinations
 
         Naming:
@@ -81,37 +91,72 @@ class GUI:
         Row 2, column 4 = disp24
         etc.
         """
-        # this loop creates the 15 labels
-        for each in range(24):
+        # this loop creates the names of the image labels
+        for each in range(96):
             #first the individual names are created and appended to the list 'lstDisplayLBLNames'
-            if each < 6:
+            if each < 12:
                 name = 'disp' + '0' +str(each)
-            elif each <12:
-                name = 'disp' + '1' + str(each - 6)
-            elif each < 18:
-                name = 'disp' + '2' + str(each - 12)
+            elif each < 24:
+                name = 'disp' + '1' + str(each - 12)
+            elif each < 36:
+                name = 'disp' + '2' + str(each - 24)
+            elif each < 48:
+                name = 'disp' + '3' + str(each - 36)
+            elif each < 60:
+                name = 'disp' + '4' + str(each - 48)
+            elif each < 72:
+                name = 'disp' + '5' + str(each - 60)
+            elif each < 84:
+                name = 'disp' + '6' + str(each - 72)
             else:
-                name = 'disp' + '3' + str(each - 18)
+                name = 'disp' + '7' + str(each - 84)
 
-            self.lstDisplayLBLNames.append(name)
+            lstDisplayLBLNames.append(name)
+            print name
 
             # the list is used to create a set of labels
-            self.lstDisplayLBLNames[each] = Label(self.frWindow, image=self.blank, width=90, height=90, relief=SUNKEN)
+            lstDisplayLBLNames[each] = Label(self.frWindow,image = self.blank, width=47, height=47, relief=SUNKEN)
 
-            # every second column has x direction padding to easily see which sector the Sphero is located in
-            if (int(name[-1]) + 1)  % 2 == 0:
-                self.lstDisplayLBLNames[each].grid(row=int(name[-2]) + 2, column=int(name[-1]) + 2, padx = (1,7.5))
-            # every label in the second row has padding on the bottom to created 3 clear sectors
-            if int(name[-2]) == 1:
-                self.lstDisplayLBLNames[each].grid(row=int(name[-2]) + 2, column=int(name[-1]) + 2, pady=(1, 7.5))
-            # labels not in columns 1 or 3 or in the second row are simply given a grid position
+            # a length checker is necessary as labels in the 12th row are have 3 digits in their name
+            if len(name) == 6:
+                lstDisplayLBLNames[each].grid(row = int(name[-2]) + 2, column= int(name[-1]) + 2, padx=(2,2), pady=(2,2))
+                if int(name[-1]) == 0:
+                    lstDisplayLBLNames[each].grid(row=int(name[-2]) + 2, column=int(name[-1]) + 2, padx=(25, 2), pady=(2,2))
             else:
-                self.lstDisplayLBLNames[each].grid(row = int(name[-2]) + 2, column= int(name[-1]) + 2)
-
+                lstDisplayLBLNames[each].grid(row = int(name[-3]) + 2, column = int(name[-1]) + 12,padx=(2,2), pady=(2,2))
 
 
 
         self.frWindow.update()
+
+# quits program, closes window
+    def fnQuit(self):
+        self.frWindow.destroy()
+        self.master.destroy()
+
+# when return is selected in menubar, the window closes and module 1 is opened
+    def fnReturnModule1(self):
+        pass
+
+    def fnTimeElapsed(self):
+        # starts the timer displayed in the time elapsed label
+
+        now = time.time()
+        time_elapsed = now - start
+        self.lblTimeElapsed.config(text=round(time_elapsed,2))
+        wdBaseWindow.after(1000, self.fnTimeElapsed)
+
+    def fnStart(self):
+        # call the sphero start command in the functions file
+        fn.fnUpdateDisplay()
+
+        start = time.time()
+        global start
+        self.fnTimeElapsed()
+
+
+
 wdBaseWindow = Tk()
 appBasicGUI = GUI(wdBaseWindow)
 wdBaseWindow.mainloop()
+
